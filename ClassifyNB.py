c978b2d92ab095be3ff8bbefdd7fa4bfb472d19d
Anderson Weber <39283833+andweber92@users.noreply.github.com>
2018-10-05 23:47:56 -0300

    Add files via upload

---
+++ ClassifyNB.py
@@ -0,0 +1,13 @@
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    ### create classifier
+    clf = GaussianNB()
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+    return clf.fit(features_train, labels_train)
+
+    
+  
+ 
+    
